e3add589581a1461b34c6bce35fea2537d6afeec
uncleeugene <uncle.eugene@gmail.com>
2023-12-22 16:01:43 +0600

    enttec OpenDMX output implemented using goftdi

--- go.mod
+++ go.mod
@@ -1,12 +1,23 @@
-module ssacndmx
+module sacndmx
 
 go 1.21.5
 
 require (
+	github.com/Hundemeier/go-sacn/sacn v0.0.0-20221003163232-00e6fbef50ad
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/uncleeugene/goftdi v0.0.0-20231222094304-f1c6abfe0361
+)
+
+require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
-	github.com/Hundemeier/go-sacn/sacn v0.0.0-20221003163232-00e6fbef50ad // indirect
+	github.com/amkulikov/goftdi v0.0.0-20170111134808-4ca7ef6aee50 // indirect
 	github.com/google/gousb v1.1.2 // indirect
 	github.com/oliread/usbdmx v0.0.0-20200510141510-3b43952fa44b // indirect
-	golang.org/x/net v0.0.0-20221002022538-bcab6841153b // indirect
-	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/exp v0.0.0-20231219180239-dc181d75b848 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	periph.io/x/conn/v3 v3.7.0 // indirect
+	periph.io/x/d2xx v0.1.0 // indirect
+	periph.io/x/host v0.0.1 // indirect
+	periph.io/x/host/v3 v3.8.2 // indirect
 )
--- sacndmx.go
+++ sacndmx.go
@@ -1,33 +1,175 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"log"
+	"net"
+	"os"
+	"time"
 
-	"github.com/oliread/usbdmx"
-	"github.com/oliread/usbdmx/ft232"
+	"github.com/Hundemeier/go-sacn/sacn"
+	"github.com/jessevdk/go-flags"
+	ftdi "github.com/uncleeugene/goftdi"
 )
 
-func main() {
+type enttecOpenDMX struct {
+	Device  *ftdi.Device
+	channel []byte
+	buffer  []byte
+}
 
-	vid := uint16(0x0403)
-	pid := uint16(0x6001)
-	inputInterfaceID := flag.Int("input-id", 0, "Input interface ID for device")
-	outputInterfaceID := flag.Int("output-id", 0, "Output interface ID for device")
-	debugLevel := flag.Int("debug", 0, "Debug level for USB context")
-	flag.Parse()
+var lineProperties = ftdi.LineProperties{
+	Bits:     ftdi.BITS_8,
+	StopBits: ftdi.STOP_2,
+	Parity:   ftdi.NONE,
+}
 
-	// Create a configuration from our flags
-	config := usbdmx.NewConfig(vid, pid, *inputInterfaceID, *outputInterfaceID, *debugLevel)
-
-	// Get a usb context for our configuration
-	config.GetUSBContext()
-
-	// Create a controller and connect to it
-	controller := ft232.NewDMXController(config)
-	if err := controller.Connect(); err != nil {
-		log.Fatalf("Failed to connect DMX Controller: %s", err)
-	} else {
-		log.Printf("Connected to OpenDMX!")
+func enttecOpenDMXConnect(d ftdi.DeviceInfo) (enttecOpenDMX, error) {
+	var device enttecOpenDMX
+	dev, err := ftdi.Open(d)
+	if err == nil {
+		device.Device = dev
+		device.channel = make([]byte, 512)
+		device.buffer = make([]byte, 513)
 	}
+	device.Device.SetLineProperty(lineProperties)
+	device.Device.SetBaudRate(250000)
+	device.Device.Purge()
+	return device, err
+}
+
+func (d *enttecOpenDMX) Close() error {
+	return d.Device.Close()
+}
+
+func (d *enttecOpenDMX) SetChannel(index int16, data byte) error {
+	d.channel[index-1] = data
+
+	return nil
+}
+
+func (d *enttecOpenDMX) Render() error {
+	for i := 0; i < 512; i++ {
+		d.buffer[i+1] = d.channel[i]
+	}
+	d.Device.SetBreakOn(lineProperties)
+	d.Device.SetBreakOff(lineProperties)
+	if _, err := d.Device.Write(d.buffer); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *enttecOpenDMX) Run() {
+	for {
+		if err := d.Render(); err != nil {
+			log.Fatalf("Error rendering DMX: %s\n", err)
+		}
+		time.Sleep(50 * time.Millisecond)
+	}
+}
+
+var CLIOptions struct {
+	DumpTOML bool   `short:"s" long:"showconfig" description:"Dump configuration and exit"`
+	Config   string `short:"c" long:"config" default:"sacndmx.toml" description:"Configuration file path"`
+	IPAddr   string `short:"a" long:"addr" default:"localhost" description:"Listener IP address"`
+	ListIPs  bool   `short:"i" long:"list-ips" description:"List local IPs"`
+	ListDevs bool   `short:"f" long:"list-devices" description:"List devices"`
+	Device   string `short:"d" long:"device" default:"" description:"Device serial number to connect to"`
+	Reset    bool   `short:"r" long:"reset-output" description:"Drop DMX output to zero in case of sACN timeout"`
+	DevType  string `short:"t" long:"device-type" default:"opendmx" description:"Device type. Not implemented yet."`
+}
+
+func main() {
+	var exitFlag bool
+
+	_, err := flags.Parse(&CLIOptions)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1) // Exit with code 1 if cli flags are not correct
+	}
+
+	dl, err := ftdi.GetDeviceList()
+	if err == nil {
+		if CLIOptions.ListDevs {
+			for i := 0; i < len(dl); i++ {
+				fmt.Printf("Device %d: S/N %s, Desc: \"%s\"\n", i, dl[i].SerialNumber, dl[i].Description)
+			}
+			exitFlag = true
+		}
+	} else {
+		log.Fatal(err)
+		exitFlag = true
+	}
+
+	recv, err := sacn.NewReceiverSocket(CLIOptions.IPAddr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if CLIOptions.ListIPs {
+		addrs, err := net.InterfaceAddrs()
+		if err == nil {
+			for _, address := range addrs {
+				// check the address type and if it is not a loopback the display it
+				if ipnet, ok := address.(*net.IPNet); ok {
+					if ipnet.IP.To4() != nil {
+						fmt.Printf("Local addr: %s\n", ipnet.IP.String())
+					}
+				}
+			}
+		}
+		exitFlag = true
+	}
+
+	if exitFlag {
+		os.Exit(2)
+	}
+
+	log.Println("sACN-DMX is starting...")
+	var devIndex int
+	var devFound bool
+	if CLIOptions.Device != "" {
+		for i := range dl {
+			if dl[i].SerialNumber == CLIOptions.Device {
+				devIndex = i
+				devFound = true
+			}
+		}
+		if !devFound {
+			log.Printf("Cannot find a device with S/N %s. Fallback to default...", CLIOptions.Device)
+			devIndex = 0
+		}
+	} else {
+		devIndex = 0
+	}
+	dmx, err := enttecOpenDMXConnect(dl[devIndex])
+	if err == nil {
+		log.Printf("Using %s (S/N %s)\n", dl[devIndex].Description, dl[devIndex].SerialNumber)
+		defer dmx.Close()
+	} else {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	go dmx.Run()
+	recv.SetOnChangeCallback(func(old sacn.DataPacket, newD sacn.DataPacket) {
+		data := newD.Data()
+		for i := 0; i < len(data); i++ {
+			dmx.SetChannel((int16)(i+1), data[i])
+		}
+	})
+	recv.SetTimeoutCallback(func(univ uint16) {
+		log.Println("Timeout detected on universe", univ)
+		// Drop all DMX channels to zero on timeout
+		if CLIOptions.Reset {
+			for i := 0; i < 512; i++ {
+				dmx.SetChannel((int16)(i+1), 0)
+			}
+		}
+	})
+	recv.Start()
+	log.Printf("sACN listener started on %s.\n", CLIOptions.IPAddr)
+
+	select {}
 }
